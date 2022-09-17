9fc1330e4d7691a0e29fcdc6d80eb9e4a6ec20ef
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-09-17 21:29:42 +0900

    xox: :sparkles: Add value

--- src/useMap/useMap.ts
+++ src/useMap/useMap.ts
@@ -1,14 +1,11 @@
 const o = <T>(p: T) => ({
   map: <F extends (p: T) => ReturnType<F>>(f: F) => o(f(p)),
+  value: p,
 });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Fn = (...arg: any) => any;
-type UseMap = <Hook extends Fn>(p: {
-  useHook: Hook;
-}) => {
-  map: ReturnType<typeof o>["map"];
-};
+type UseMap = <Hook extends Fn>(p: { useHook: Hook }) => ReturnType<typeof o>;
 
 export const useMap: UseMap = ({ useHook }) => ({
   ...o(useHook()),
